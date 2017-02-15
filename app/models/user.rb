b9f432918f9da4facb1e8fef13db6a1af3b236dc
Marion Neuenschwander <ionamaron@gmail.com>
2017-02-15 23:01:09 +0100

    missing self.search method in models (user), required input new user, unification of error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :first_name, :last_name, :email, :presence => true
+
+  def self.search(query)
+    where "first_name LIKE :q OR last_name LIKE :q OR email LIKE :q", {q: '%'+query+'%'}
+  end
+
   def admin?
     role == 'admin'
   end
